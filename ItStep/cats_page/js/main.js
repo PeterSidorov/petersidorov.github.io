c72eb2ebdecb1c991c316c1fe88348ff6339b40c
sombartu <petya007s@gmail.com>
2024-03-14 05:04:59 +0200

    new site 3D-forest

---
+++ ItStep/cats_page/js/main.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('btn');
+let modal = document.getElementById('window');
+let cat = document.getElementById('catImg');
+btn.addEventListener('click', function () {
+    modal.classList.toggle('active');
+})
+cat.addEventListener('click', function () {
+    modal.classList.toggle('active');
+})
