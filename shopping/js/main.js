96fb4d1317a61abd692a45131669bb9eee3b4471
sombartu <petya007s@gmail.com>
2024-03-20 04:23:19 +0200

    new site 3D-forest

--- shopping/js/main.js
+++ shopping/js/main.js
@@ -1,8 +1,12 @@
 const list = document.getElementById('list');
 const btn = document.getElementById('btn');
 const modalWindow = document.getElementById('modal-window');
+const congratulation = document.getElementById('congratulation-block');
 let items = document.querySelectorAll('.item');
 let itemsText = document.querySelectorAll('.item_text');
+const repeatList = document.querySelector('.repeat__list');
+
+/**/
 
 /**/
 
@@ -10,45 +14,64 @@ let itemsText = document.querySelectorAll('.item_text');
 list.addEventListener( 'input', function () {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight + 'px';
-});
+}); /* Решение что-бы textarea подстраивалась под размер текста */
 
 let val; // Список
+
 let newVal; // Массив из списка
 
 function main () {
     val = list.value;
-    newVal = val.split('\n'); //Делит список продуктов на массив
+    // В переменной val сохранен текст который ввели в list
+    // list.value это весь текст который ввели в textarea на странице
+    // Далее текст делиться на ячейки массива с помощью
+    newVal = val.split('\n'); // Делит список(весь текст) на массив с помощью .split('\n');
     for (let a=0; a < newVal.length;a++) {
         itemsText[a].innerHTML = "" + newVal[a];
         items[a].classList.add('displayActive')
-    }
-    if (newVal[0] === "") {
-        console.log('гы гы гы')
-    }
+    } /* Этот цикл for: Каждому айтему в текст добавляет
+         текст который указали в textarea и разделили на ячейки массива
+         Например текст первого айтема = первой ячейке массива
+         и добавляет ячейке массива класс 'displayActive' */
 }
 
-
-
+/* При нажатии на кнопку стартуют события */
 btn.addEventListener('click', function () {
 
     for (let a=0; a < 100;a++) {
         items[a].classList.remove('displayActive')
-    }
+    } /* При нажатии на кнопку,
+         удаляет класс 'displayActive' всем айтемам */
+
     main();
+
     for (let a=0; a < 100;a++) {
         if (newVal[a] === "") {
             items[a].classList.remove('displayActive')
         }
-    }
-    if (newVal[0] === "") {
+    } /* newVal = весь массив, со всеми ячейками
+         цикл for прогоняет все айтемы массива
+         и если есть пустые айтемы удаляет у них класс 'displayActive' */
+
+    if (val === "") {
+        console.log('Список должен содержать хотя бы один продукт')
+    } else if (newVal.join('') === ""){
         console.log('Список должен содержать хотя бы один продукт')
     } else {
         modalWindow.classList.add('close')
-    }
+        console.log(newVal)
+        console.log(newVal.join(''))
+    } /* Проверяет textarea на наличие текста
+       Если в textarea пусто, пишет в консоль что нужно добавить продукты,
+       если текст есть, закрывает модальное окно */
+
+
+
 })
 
 
-function clickRemoveActive (favicon) {
+/* Удаляет айтем продукта при двойном клике */
+function dblclickRemoveActive (favicon) {
     for (let a=0;a<100;a++) {
         favicon[a].addEventListener( "dblclick", function (){
             // console.log('hello', a)
@@ -56,52 +79,45 @@ function clickRemoveActive (favicon) {
         })
     }
 }
-
-clickRemoveActive(items);
-
+dblclickRemoveActive(items); /* Удаляет айтем продукта при двойном клике */
 
 
 
+/* Блок проверки сколько осталось айтемов с активным классом displayActive
+   если нет блоков с активным классом displayActive в консоли пишет 'Поздравляю вы завершили покупки' */
 
+repeatList.innerHTML = "";
 
+for (let a = 0; a<100; a++) {
+    items[a].addEventListener("dblclick", function () {
+        let objectItemDisplayActive = document.querySelectorAll('.item.displayActive');
+        console.log(objectItemDisplayActive.length)
 
-/*
-let spisok =
-    "Хлеб\n" +
-    "Лаваш\n" +
-    "Сулугуни 2 шт\n" +
-    "Бри 2 кусочка\n" +
-    "Помидоры \n" +
-    "Огурцы\n" +
-    "Лимоны\n" +
-    "Пакеты упаковка маленькие\n" +
-    "Пакеты для мусора\n" +
-    "Сок Эмми Клубника банан 2 шт\n" +
-    "Кола 3 шт\n" +
-    "Круассаны\n" +
-    "Крылышки 3 уп\n" +
-    "Масло сливочное 3 пачки\n" +
-    "Тортик без Орехов\n" +
-    "Мороженое на палочке пломбир в шоколаде эскимос";
+        if (objectItemDisplayActive.length === 0) {
+            console.log('Поздравляю вы завершили покупки')
+            congratulation.classList.remove('displayNone');
 
- let masivSpisok = spisok.split('\n');
+            let filteredNewVal = newVal.filter(Boolean);
 
- console.log(spisok);
- console.log(masivSpisok);
- console.log(masivSpisok[0]);
- console.log(masivSpisok[3]);
- console.log(masivSpisok[7]);
- */
+            filteredNewVal.forEach((book, i) => {
+                repeatList.innerHTML += `<li class="repeat__item">${i + 1} ${book}</li>`;
+            });
 
+        }
+    })
+}
 
 
 /*
-const item_text__zero = document.getElementById('item_text__zero');
-const item_text__uno = document.getElementById('item_text__uno');
-const item_text__due = document.getElementById('item_text__due');
-const item_text__tre = document.getElementById('item_text__tre');
-item_text__zero.innerHTML="" + newVal[0];
-item_text__uno.innerHTML="" + newVal[1];
-item_text__due.innerHTML="" + newVal[2];
-item_text__tre.innerHTML="" + newVal[3];
+if (val === "") {
+        console.log('Список должен содержать хотя бы один продукт')
+    } else {
+        modalWindow.classList.add('close')
+        console.log(newVal)
+        console.log(newVal.join(''))
+    }
  */
+
+/* Проверяет textarea на наличие текста
+       Если в textarea пусто, пишет в консоль что нужно добавить продукты,
+       если текст есть, закрывает модальное окно */
