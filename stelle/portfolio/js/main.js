ae563d164e7db815b6705ea09d2fbbd1bee1e9b8
sombartu <petya007s@gmail.com>
2024-04-24 05:32:07 +0300

    88888888

--- stelle/portfolio/js/main.js
+++ stelle/portfolio/js/main.js
@@ -1,8 +1,6 @@
 const change = document.getElementById('changeImg');
 const header = document.getElementById('header');
 
-console.log(change);
-console.log(header);
 
 change.addEventListener("click", function (){
     header.classList.toggle('new-img');
